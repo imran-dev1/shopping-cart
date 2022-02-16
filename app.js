706b06da88f64a3a5be5fbe0843ffbe260c1348d
webdev-it <webdevit3@gmail.com>
2022-02-16 08:51:49 +0600

    Cart quantity and price updated

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('I am in a separate file')
+// Quantity increment decrement
+function qtyAdd() {
+    const qtyInput = document.getElementById('quantity');
+    let qtyInputValue = qtyInput.value;
+    qtyInput.value = parseInt(qtyInputValue) + 1;
+    const itemPrice = document.getElementById('item-price');
+    let itemPriceValue = 1150;
+    itemPrice.innerText = itemPriceValue * qtyInput.value;
+}
+function qtyMinus() {
+    let qtyInput = document.getElementById('quantity');
+    let qtyInputValue = qtyInput.value;
+    if (qtyInputValue>0) {
+        qtyInput.value = parseInt(qtyInputValue) - 1;
+        const itemPrice = document.getElementById('item-price');
+        let itemPriceValue = 1150;
+        itemPrice.innerText = itemPriceValue * qtyInput.value;
+        itemPriceValue = itemPrice.innerText;
+
+    }
+    
+}
